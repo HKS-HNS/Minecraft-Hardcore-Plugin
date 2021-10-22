b48868e4c8133229f6b69d2f5bbe40423eb8bfce
HKS-HNS <elias.fit@outlook.de>
2021-10-22 21:53:15 +0200

    Added Updater to onEnable Method

--- Hardcore/src/me/HKS/HNS/Main.java
+++ Hardcore/src/me/HKS/HNS/Main.java
@@ -4,23 +4,33 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.HKS.HNS.Listeners.Config;
 import me.HKS.HNS.Screen.Actionbar;
 
 public class Main extends JavaPlugin {
+	private static Main Instance;
     Actionbar Ac = new Actionbar(); // Set's the Actionbar
     @Override
-    public void onEnable() {
+    public void onEnable() { 
+    	PluginDescriptionFile pdf = this.getDescription();
+ 
         this.getServer().getPluginManager().registerEvents((Listener) new Config(), (Plugin) this);
         this.getCommand("Hardcore").setExecutor((CommandExecutor) new Config());
         this.getCommand("Hardcore").setTabCompleter((TabCompleter) new Config());
         Ac.Start();
-    }
+        new Updater(pdf, this.getFile());
 
+    }
+ 
     @Override
     public void onDisable() {
         Ac.DelAC();
     }
+    
+    
+	public static Main getInstance() {return Instance;}
+
 }
