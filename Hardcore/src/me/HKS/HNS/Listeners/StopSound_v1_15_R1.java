61082d283ad74763548a309991512ebe54f32752
HKS-HNS <elias.fit@outlook.de>
2021-10-29 23:39:14 +0200

    Removed debug messages

--- Hardcore/src/me/HKS/HNS/Listeners/StopSound_v1_15_R1.java
+++ Hardcore/src/me/HKS/HNS/Listeners/StopSound_v1_15_R1.java
@@ -3,7 +3,6 @@ package me.HKS.HNS.Listeners;
 import java.lang.reflect.Field;
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
@@ -34,40 +33,40 @@ public class StopSound_v1_15_R1 implements Listener, StopSoundInf {
     }
 
     @EventHandler
-    public void onLeave(PlayerQuitEvent e ) {
+    public void onLeave(PlayerQuitEvent e) {
         removePlayer(e.getPlayer());
     }
-    
+
     @EventHandler
-    public void onSwitchSlot(PlayerItemHeldEvent e ) {
-    	Player p = e.getPlayer();
-    	ItemStack Frod = new ItemStack(Material.FISHING_ROD);
+    public void onSwitchSlot(PlayerItemHeldEvent e) {
+        Player p = e.getPlayer();
+        ItemStack Frod = new ItemStack(Material.FISHING_ROD);
         ItemMeta itemMeta = Frod.getItemMeta();
         itemMeta.setUnbreakable(true);
         Frod.setItemMeta(itemMeta);
-    	if (p.getWorld().getName().equalsIgnoreCase("DeathPlayerWorld")) {
-    		if (p.getInventory().getItem(e.getPreviousSlot()) != null)
-           if (p.getInventory().getItem(e.getPreviousSlot()).equals(Frod) ) {
-        	   if (playersFishing.contains(p.getUniqueId())) 
-        	   playersFishing.remove(p.getUniqueId());
-           }
+        if (p.getWorld().getName().equalsIgnoreCase("DeathPlayerWorld")) {
+            if (p.getInventory().getItem(e.getPreviousSlot()) != null)
+                if (p.getInventory().getItem(e.getPreviousSlot()).equals(Frod)) {
+                    if (playersFishing.contains(p.getUniqueId()))
+                        playersFishing.remove(p.getUniqueId());
+                }
 
-    	}
+        }
     }
     @EventHandler
     public void onFish(PlayerFishEvent e) {
-    	Player p = e.getPlayer();
+        Player p = e.getPlayer();
         if (p.getWorld().getName().equalsIgnoreCase("DeathPlayerWorld")) {
             State fishst = e.getState();
             UUID PlayerUUID = p.getUniqueId();
             if (fishst.equals(PlayerFishEvent.State.FISHING)) {
-            	if (!playersFishing.contains(PlayerUUID)) 
-                playersFishing.add(PlayerUUID);
-            	if (!playerThrown.contains(PlayerUUID)) 
-                playerThrown.add(PlayerUUID);
+                if (!playersFishing.contains(PlayerUUID))
+                    playersFishing.add(PlayerUUID);
+                if (!playerThrown.contains(PlayerUUID))
+                    playerThrown.add(PlayerUUID);
             } else if (fishst.equals(PlayerFishEvent.State.IN_GROUND) || fishst.equals(PlayerFishEvent.State.REEL_IN)) {
-            	if (!playerReelin.contains(PlayerUUID)) 
-            	playerReelin.add(PlayerUUID);
+                if (!playerReelin.contains(PlayerUUID))
+                    playerReelin.add(PlayerUUID);
             }
 
         }
@@ -92,49 +91,45 @@ public class StopSound_v1_15_R1 implements Listener, StopSoundInf {
             public void write(ChannelHandlerContext channelHandlerContext, Object packet, ChannelPromise promise) throws Exception {
                 if (packet instanceof PacketPlayOutNamedSoundEffect) {
                     PacketPlayOutNamedSoundEffect packetSoundNamed = (PacketPlayOutNamedSoundEffect) packet;
-                    if (p.getWorld().getName().equalsIgnoreCase("DeathPlayerWorld")) 
-                    {
-                    try {
-                    	
-                    	Field fila = packetSoundNamed.getClass().getDeclaredField("a");
-            			fila.setAccessible(true);
-            			SoundEffect a = (SoundEffect) fila.get(packetSoundNamed);
-                        UUID PlayerUUID = p.getUniqueId();                        
-                    	 if (a.equals(SoundEffects.ENTITY_FISHING_BOBBER_RETRIEVE) || a.equals(SoundEffects.ENTITY_FISHING_BOBBER_SPLASH) || a.equals(SoundEffects.ENTITY_FISHING_BOBBER_THROW) || a.equals(SoundEffects.ENTITY_GENERIC_SPLASH)) {
-     						Bukkit.getLogger().info("Reeling: "+playerReelin.contains(PlayerUUID) +" Throw:" +playerThrown.contains(PlayerUUID)+ " Fishing: "+playersFishing.contains(PlayerUUID)+" Name: "+ p.getName() );
+                    if (p.getWorld().getName().equalsIgnoreCase("DeathPlayerWorld")) {
+                        try {
 
-                             if (playerReelin.contains(PlayerUUID)) {
-                                 playerReelin.remove(PlayerUUID);
-                                 playersFishing.remove(PlayerUUID);
-                                 Bukkit.getLogger().info("eee");
-                                 if (!a.equals(SoundEffects.ENTITY_FISHING_BOBBER_RETRIEVE)) {
-                                 	
-                                     return;
-                                 }
+                            Field fila = packetSoundNamed.getClass().getDeclaredField("a");
+                            fila.setAccessible(true);
+                            SoundEffect a = (SoundEffect) fila.get(packetSoundNamed);
+                            UUID PlayerUUID = p.getUniqueId();
+                            if (a.equals(SoundEffects.ENTITY_FISHING_BOBBER_RETRIEVE) || a.equals(SoundEffects.ENTITY_FISHING_BOBBER_SPLASH) || a.equals(SoundEffects.ENTITY_FISHING_BOBBER_THROW) || a.equals(SoundEffects.ENTITY_GENERIC_SPLASH)) {
 
-                             } else if (playerThrown.contains(PlayerUUID)) {
+                                if (playerReelin.contains(PlayerUUID)) {
+                                    playerReelin.remove(PlayerUUID);
+                                    playersFishing.remove(PlayerUUID);
+                                    if (!a.equals(SoundEffects.ENTITY_FISHING_BOBBER_RETRIEVE)) {
 
-                                 playerThrown.remove(PlayerUUID);
-                                 if (!a.equals(SoundEffects.ENTITY_FISHING_BOBBER_THROW)) {
-                                     return;
-                                 }
-                             } else if (playersFishing.contains(PlayerUUID)) {
-                                 if (!a.equals(SoundEffects.ENTITY_GENERIC_SPLASH) && !a.equals(SoundEffects.ENTITY_FISHING_BOBBER_SPLASH)) {
-                                     return;
-                                 }
-                             } else {
-                                 return;
-                             }
-                             
-                         }
-                    	    fila.setAccessible(false);
-					} catch (Exception e) {
-						e.printStackTrace();
-						return;
-					}
-					}
-                    
-                   
+                                        return;
+                                    }
+
+                                } else if (playerThrown.contains(PlayerUUID)) {
+
+                                    playerThrown.remove(PlayerUUID);
+                                    if (!a.equals(SoundEffects.ENTITY_FISHING_BOBBER_THROW)) {
+                                        return;
+                                    }
+                                } else if (playersFishing.contains(PlayerUUID)) {
+                                    if (!a.equals(SoundEffects.ENTITY_GENERIC_SPLASH) && !a.equals(SoundEffects.ENTITY_FISHING_BOBBER_SPLASH)) {
+                                        return;
+                                    }
+                                } else {
+                                    return;
+                                }
+
+                            }
+                            fila.setAccessible(false);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return;
+                        }
+                    }
+
                 }
                 super.write(channelHandlerContext, packet, promise);
             }
